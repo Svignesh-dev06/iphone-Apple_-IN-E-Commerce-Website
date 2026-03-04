81934cff08eec91d246848d12bbeab8746ab4e89
Svignesh-dev06 <subramaniankv68@gmail.com>
2026-03-04 14:28:34 +0530

    iOS,index,iphone-17-max,script.js.style.css file modifyed

--- Main-Page/script.js
+++ Main-Page/script.js
@@ -19,21 +19,43 @@ let elements = document.querySelectorAll(".explore-anmition, .navimg-anmition, .
             } }, {threshold: 0.9}) })
 
  let blurcontainer = document.getElementById("blur-container")
+ let blurcontainer2=document.getElementById("blur-container2")
+
  let shownav = document.getElementById("store")
+ let shownav2 =document.getElementById("mac")
+
  let storenavcontainer = document.getElementById("store-navcontainer")
+ let macnavcontainer =document.getElementById("mac-navcontainer")
 
   shownav.addEventListener("mouseenter", function(){
    blurcontainer.style.display="block"
    blurcontainer.style.height="100%"
-  })
+  });
   shownav.addEventListener("mouseleave", function(){
    blurcontainer.style.display="none"
   });
+
+  shownav2.addEventListener("mouseenter", function(){
+   blurcontainer2.style.display="block"
+   blurcontainer2.style.height="100%"
+   blurcontainer2.style.transform="trans"
+  });
+  shownav2.addEventListener("mouseleave", function(){
+   blurcontainer2.style.display="none"
+  })
+
 
   storenavcontainer.addEventListener("mouseenter", function(){
    blurcontainer.style.display ="block"
   });
   storenavcontainer.addEventListener("mouseleave", function(){
    blurcontainer.style.display = "none"
+  });
+
+  macnavcontainer.addEventListener("mouseenter" , function(){
+   blurcontainer2.style.display="block"
+  })
+  macnavcontainer.addEventListener("mouseleave", function(){
+   blurcontainer2.style.display="none"
   })
             
